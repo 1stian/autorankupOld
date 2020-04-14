02f43d1a15e04ec0b672656b8bdc1437f9274ea9
Stian Tofte <stian@realnaits.com>
2020-04-14 19:32:19 +0200

    Initial commit

---
+++ autorankup-spigot/src/main/java/com/ohneemc/autorankup/AutoRankUpSpigot.java
@@ -0,0 +1,17 @@
+package com.ohneemc.autorankup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoRankUpSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
